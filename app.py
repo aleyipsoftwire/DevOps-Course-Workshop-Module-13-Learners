50fe8877afcf3bf3beeaf5be0327a9e8229df08e
Alex Yip <alex.yip@softwire.com>
2025-01-28 14:54:52 +0000

    Add app insights

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import logging
 
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+logging.basicConfig(level=logging.INFO)
+configure_azure_monitor()
+
 from flask import Flask, render_template, request
 from datetime import datetime, timezone
 
 from werkzeug.utils import redirect
 from flask_config import Config
-from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
+from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, \
+    get_queued_count, get_recently_placed_count, get_recently_processed_count
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 initialise_database(app)
 initialise_scheduled_jobs(app)
-
-logging.basicConfig(level=logging.INFO)
 
 
 @app.route("/")
@@ -25,11 +30,11 @@ def index():
     recently_placed_count = get_recently_placed_count()
     recently_processed_count = get_recently_processed_count()
     scenarios = [
-        { 'display': 'Add broken order', 'value': 'BrokenOrder' },
-        { 'display': 'Monitoring Load', 'value': 'HighLoad' },
-        { 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
-        { 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
-        { 'display': 'Reset to initial', 'value': 'Reset' }
+        {'display': 'Add broken order', 'value': 'BrokenOrder'},
+        {'display': 'Monitoring Load', 'value': 'HighLoad'},
+        {'display': 'Queue Reliability', 'value': 'UnreliableProcessing'},
+        {'display': 'System Monitoring', 'value': 'VeryHighLoad'},
+        {'display': 'Reset to initial', 'value': 'Reset'}
     ]
 
     return render_template(
@@ -37,11 +42,12 @@ def index():
         recently_processed_count=recently_processed_count, scenarios=scenarios
     )
 
+
 @app.route("/count")
 def count():
     order_count = count_orders()
     app.logger.info(f'Counted {order_count} orders')
-    return { 'count': order_count }
+    return {'count': order_count}
 
 
 @app.route("/new", methods=["POST"])
@@ -79,6 +85,7 @@ def set_scenario():
     response.raise_for_status()
 
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.run()
