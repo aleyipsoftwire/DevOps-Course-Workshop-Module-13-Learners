7f893c3e11ff47993702fe76cb1153d3cbe6ab05
Alex Yip <alex.yip@softwire.com>
2025-01-28 11:54:43 +0000

    Add actual useful log

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config.from_object(Config)
 initialise_database(app)
 initialise_scheduled_jobs(app)
 
-logging.basicConfig(level=logging.INFO) # QQ
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route("/")
