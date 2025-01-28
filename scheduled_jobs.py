95bad17d8409a0e49d5b5b5844cefaaca3768457
Alex Yip <alex.yip@softwire.com>
2025-01-28 12:01:10 +0000

    Log payload

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -29,6 +29,8 @@ def process_orders(app):
             "date": order.date_placed.isoformat(),
         }
 
+        app.logger.info(f"Request payload: {payload}")
+
         response = requests.post(
             app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
             json=payload
